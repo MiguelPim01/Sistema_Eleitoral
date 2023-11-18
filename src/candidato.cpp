9ec75c8909618f59dd84b24b1de2f702093f8629
Miguel <miguelmip02@gmail.com>
2023-11-17 23:01:14 -0300

    variaveis adicionadas as classes e getters feitos

--- src/candidato.cpp
+++ src/candidato.cpp
@@ -5,6 +5,56 @@ Candidato::Candidato()
 
 }
 
+int Candidato::get_cd_cargo()  const
+{
+    return this->cd_cargo;
+}
+
+int Candidato::get_cd_situacao_cadidato_tot()  const
+{
+    return this->cd_sit_tot_turno;
+}
+
+int Candidato::get_nr_candidato()  const
+{
+    return this->nr_candidato;
+}
+
+string Candidato::get_nm_urna_candidato()  const
+{
+    return this->nm_urna_candidato;
+}
+
+Partido& Candidato::get_partido()  const
+{
+    return *(this->p);
+}
+
+int Candidato::get_nr_federacao()  const
+{
+    return this->nr_federacao;
+}
+
+int Candidato::get_cd_sit_tot_turno()  const
+{
+    return this->cd_sit_tot_turno;
+}
+
+int Candidato::get_cd_genero()  const
+{
+    return this->cd_genero;
+}
+
+string Candidato::get_nm_tipo_destinacao_votos()  const
+{
+    return this->nm_tipo_destinacao_votos;
+}
+
+int Candidato::get_votos_nominais()  const
+{
+    return this->votos_nominais;
+}
+
 Candidato::~Candidato()
 {
     
--- src/partido.cpp
+++ src/partido.cpp
@@ -5,6 +5,31 @@ Partido::Partido()
 
 }
 
+int Partido::get_nr_partido() const
+{
+    return this->nr_partido;
+}
+
+string Partido::get_sg_partido()  const
+{
+    return this->sg_partido;
+}
+
+int Partido::get_votos_nominais()  const
+{
+    return this->votos_nominais;
+}
+
+int Partido::get_votos_de_legenda()  const
+{
+    return this->votos_de_legenda;
+}
+
+int Partido::get_qtd_cd_eleitos()  const
+{
+    return this->qtd_cd_eleitos;
+}
+
 Partido::~Partido()
 {
     
